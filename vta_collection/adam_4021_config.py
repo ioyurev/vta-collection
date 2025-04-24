b057751fb2c32796c352ad368c02709245117f2d
Илья Олегович Юрьев <i.o.yurev@yandex.ru>
2025-04-24 23:33:34 +0500

    feat(adam_4021_config.py): add adam4021 config module and fix address formatting in adam4011

--- vta_collection/adam_4021_config.py
+++ vta_collection/adam_4021_config.py
@@ -2,43 +2,19 @@ from enum import Enum
 from typing import NamedTuple
 
 
-class IRC(Enum):
-    C00 = "00"
-    C01 = "01"
-    C02 = "02"
-    C03 = "03"
-    C04 = "04"
-    C05 = "05"
-    C06 = "06"
-    C07 = "07"
-    C0E = "0E"
-    C0F = "0F"
-    C10 = "10"
-    C11 = "11"
-    C12 = "12"
-    C13 = "13"
-    C14 = "14"
+class ORC(Enum):
+    C30 = "30"
+    C31 = "31"
+    C32 = "32"
 
     def description(self):
         return INPUTRANGE_CODES[self]
 
 
-INPUTRANGE_CODES: dict[IRC, str] = {
-    IRC.C00: "± 15 mV",
-    IRC.C01: "± 50 mV",
-    IRC.C02: "± 100 mV",
-    IRC.C03: "± 500 mV",
-    IRC.C04: "± 1 V",
-    IRC.C05: "± 2.5 V",
-    IRC.C06: "20 mA",
-    IRC.C07: "4~20mA",
-    IRC.C0E: "Type J Thermocouple 0 - 760 °C",
-    IRC.C0F: "Type K Thermocouple 0 - 1370 °C",
-    IRC.C10: "Type T Thermocouple 100 - 400 °C",
-    IRC.C11: "Type E Thermocouple 0 - 1000 °C",
-    IRC.C12: "Type R Thermocouple 500 - 1750 °C",
-    IRC.C13: "Type S Thermocouple 500 - 1750 °C",
-    IRC.C14: "Type B Thermocouple 500 - 1800 °C",
+INPUTRANGE_CODES: dict[ORC, str] = {
+    ORC.C30: "0 to 20 mA",
+    ORC.C31: "4 to 20 mA",
+    ORC.C32: "0 to 10 V",
 }
 
 
@@ -64,20 +40,6 @@ BAUDRATE_CODES: dict[BC, int] = {
 }
 
 
-class ITC(Enum):
-    C0 = "0"
-    C1 = "1"
-
-    def description(self):
-        return INTEGRATIONTIME_CODES[self]
-
-
-INTEGRATIONTIME_CODES: dict[ITC, str] = {
-    ITC.C0: "50 ms (Operation under 60 Hz power)",
-    ITC.C1: "60 ms (Operation under 50 Hz power)",
-}
-
-
 class CC(Enum):
     C0 = "0"
     C1 = "1"
@@ -96,7 +58,6 @@ class DFC(Enum):
     C00 = "00"
     C01 = "01"
     C10 = "10"
-    C11 = "11"
 
     def description(self):
         return DATAFORMAT_CODES[self]
@@ -105,26 +66,59 @@ class DFC(Enum):
 DATAFORMAT_CODES: dict[DFC, str] = {
     DFC.C00: "Engineering units",
     DFC.C01: "% of FSR",
-    DFC.C10: "two's complement of hexadecimal",
-    DFC.C11: "Ohms (for 4013 and 4015)",
+    DFC.C10: "hexadecimal",
+}
+
+
+class SRC(Enum):
+    C0000 = "0000"
+    C0001 = "0001"
+    C0010 = "0010"
+    C0011 = "0011"
+    C0100 = "0100"
+    C0101 = "0101"
+    C0110 = "0110"
+    C0111 = "0111"
+    C1000 = "1000"
+    C1001 = "1001"
+    C1010 = "1010"
+    C1011 = "1011"
+
+    def description(self):
+        return SLEW_RATE_CODES[self]
+
+
+SLEW_RATE_CODES: dict[SRC, str] = {
+    SRC.C0000: "Immediate change",
+    SRC.C0001: "0.0625 V/sec 0.125 mA/sec",
+    SRC.C0010: "0.125 V/sec 0.250 mA/sec",
+    SRC.C0011: "0.250 V/sec 0.500 mA/sec",
+    SRC.C0100: "0.500 V/sec 1.000 mA/sec",
+    SRC.C0101: "1.000 V/sec 2.000 mA/sec",
+    SRC.C0110: "2.000 V/sec 4.000 mA/sec",
+    SRC.C0111: "4.000 V/sec 8.000 mA/sec",
+    SRC.C1000: "8.000 V/sec 16.00 mA/sec",
+    SRC.C1001: "16.00 V/sec 32.00 mA/sec",
+    SRC.C1010: "32.00 V/sec 64.00 mA/sec",
+    SRC.C1011: "64.00 V/sec 128.0 mA/sec",
 }
 
 
 class Codes:
-    InputRange = IRC
+    OutputRange = ORC
     Baudrate = BC
-    IntegrationTime = ITC
     Checksum = CC
+    SlewRate = SRC
     DataFormat = DFC
 
 
-class Adam4011Config(NamedTuple):
+class Adam4021Config(NamedTuple):
     address: str
     new_address: str
-    input_range: IRC
+    output_range: ORC
     baudrate: BC
-    integration_time: ITC
     checksum: CC
+    slewrate: SRC
     data_format: DFC
 
     def to_set_cmd(self):
@@ -134,14 +128,15 @@ class Adam4011Config(NamedTuple):
         cmd = (
             cmd_template.replace("AA", self.address)
             .replace("NN", self.new_address)
-            .replace("TT", self.input_range.value)
+            .replace("TT", self.output_range.value)
             .replace("CC", self.baudrate.value)
         )
 
         # Формирование битовых флагов
         ff[0:2] = list(self.data_format.value)[::-1]
+        ff[2:6] = list(self.slewrate.value)[::-1]
         ff[6] = self.checksum.value
-        ff[7] = self.integration_time.value
+        # ff[7] = self.integration_time.value
         ff = int("".join(ff[::-1]), 2)
         cmd = cmd.replace("FF", f"{ff:02X}")
 
@@ -154,24 +149,24 @@ class Adam4011Config(NamedTuple):
         codes = [string[i : i + 2] for i in range(0, len(string), 2)]
 
         if is_set_cmd:
-            address_str, new_address_str, input_range_code, baudrate_code, bits = codes
+            address_str, new_address_str, output_range_code, baudrate_code, bits = codes
         else:
-            address_str, input_range_code, baudrate_code, bits = codes
+            address_str, output_range_code, baudrate_code, bits = codes
             new_address_str = address_str
 
         address = int(address_str, 16)
         new_address = int(new_address_str, 16)
         bits = format(int(bits, 16), "08b")[::-1]
         checksum_bit = bits[6]
-        integration_time_bit = bits[7]
+        slewrate_bits = bits[5] + bits[4] + bits[3] + bits[2]
         dataformat_bits = bits[1] + bits[0]
 
-        return Adam4011Config(
+        return Adam4021Config(
             address=f"{address:02X}",
             new_address=f"{new_address:02X}",
-            input_range=IRC(input_range_code),
+            output_range=ORC(output_range_code),
             baudrate=BC(baudrate_code),
-            integration_time=ITC(integration_time_bit),
+            slewrate=SRC(slewrate_bits),
             checksum=CC(checksum_bit),
             data_format=DFC(dataformat_bits),
         )
@@ -180,15 +175,15 @@ class Adam4011Config(NamedTuple):
         return f"""AdamConfig(
     address = {self.address},
     new_address = {self.new_address},
-    input_range = {self.input_range.value}: {self.input_range.description()},
+    output_range = {self.output_range.value}: {self.output_range.description()},
     baudrate = {self.baudrate.value}: {self.baudrate.description()},
-    integration_time = {self.integration_time.value}: {self.integration_time.description()},
+    slewrate = {self.slewrate.value}: {self.slewrate.description()},
     checksum = {self.checksum.value}: {self.checksum.description()},
     dataformat = {self.data_format.value}: {self.data_format.description()}
     )"""
 
 
 if __name__ == "__main__":
-    cfg = Adam4011Config.from_str("!010206C3")  # #!AATTCCFF '%AANNTTCCFF'
+    cfg = Adam4021Config.from_str("%310A310610")  # #!AATTCCFF '%AANNTTCCFF'
     print(cfg)
     print(cfg.to_set_cmd())
